8a4b93247f33c78dbfc9c4e3488f030c6ef1a15a
M Bilal <bilalkhanshakir@gmail.com>
2024-01-20 15:17:59 +0500

    Dashboard Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      'node_modules/flowbite-react/lib/esm/**/*.js'
+    ],
+    theme: {
+      extend: {},
+    },
     plugins: [
         require('flowbite/plugin')
     ],
-
-    content: [
-        "./node_modules/flowbite/**/*.js"
-    ]
-
-}
+  }
