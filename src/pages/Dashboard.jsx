e0dff7b12faeff396a81a7cd2f9f3cb939dd51f4
mbilal-09 <bilalkhanshakir@gmail.com>
2024-01-25 17:51:13 +0500

    zip-file-debugging

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,9 +8,11 @@ import generatePDF, { Resolution, Margin } from 'react-to-pdf';
 import Barcode from 'react-barcode';
 import { randomAlphanumeric } from "random-string-alphanumeric-generator";
 import JSZip from "jszip";
+import html2pdf from 'html2pdf.js';
 
 import "../App.css";
 const options = {
+  method: 'build',
   page: {
     format: 'A5',
   },
@@ -48,23 +50,32 @@ function Dashboard() {
   };
 
   const handleDownloadAll = async () => {
+    const fileBlobs = [];
+  
+    await Promise.all(
+      csvData?.map(async (data, index) => {
+        const trackingId = randomAlphanumeric(18, "uppercase");
+        const pdfData = await generatePDF(() => document.getElementById(`content-id-${index}`), options);
+        console.log('pdfData', pdfData);
+  
+        // Convert the pdfData (which might be a DOM element or similar) to a Blob
+        const pdfBlob = new Blob([pdfData], { type: 'application/pdf' });
+  
+        fileBlobs.push({ name: `${trackingId}.pdf`, content: pdfBlob });
+      })
+    );
+  
     const zip = new JSZip();
-
-    await Promise.all(csvData.map(async (data, index) => {
-      const trackingId = randomAlphanumeric(18, "uppercase");
-      const pdfData = generatePDF(() => document.getElementById(`content-id-${index}`), options);
-
-      // Add the PDF data to the zip file
-      zip.file(`${trackingId}.pdf`, pdfData, { binary: true });
-    }));
-
-    // Generate the zip file
-    const zipBlob = await zip.generateAsync({ type: "blob" });
-
-    // Create a link element to trigger the download
-    const link = document.createElement("a");
+  
+    fileBlobs.forEach((file) => {
+      zip.file(file.name, file.content);
+    });
+  
+    const zipBlob = await zip.generateAsync({ type: 'blob' });
+  
+    const link = document.createElement('a');
     link.href = URL.createObjectURL(zipBlob);
-    link.download = "all_pdfs.zip";
+    link.download = 'slips.zip';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -125,7 +136,7 @@ function Dashboard() {
           >
             <CSVReader
               cssClass="mx-auto m-0 p-0"
-              onFileLoaded={(data, fileInfo, originalFile) =>{
+              onFileLoaded={(data, fileInfo, originalFile) => {
                 data.shift();
                 setCsvData(data)
                 console.log(data);
@@ -135,87 +146,88 @@ function Dashboard() {
         </div>
         {csvData ? (
           <button onClick={handleDownloadAll} className="bg-blue-50 text-blue-600 rounded-lg py-2 px-6 hover:bg-blue-100 border border-blue-600 m-4">
-          Download All PDFs
-        </button>
+            Download All PDFs
+          </button>
         ) : (
           ''
         )}
         <div className="flex justify-start space-x-4 p-2 overflow-x-scroll">
           {csvData ? csvData.length > 0 && csvData?.map((data, index) => {
-          let trackingId = randomAlphanumeric(18, "uppercase");
-          return(
-            <div className="w-[400px]" key={index} id={`content-id-${index}`}>
-              {/* <button onClick={() => generatePDF(getTargetElement, options)}>Download PDF</button> */}
-              <div
-               
-                className="bg-white border-[3px] border-black "
-              >
-                <div className="flex flex-row items-start justify-between p-1">
-                  <div className="text-sm ">
-                    <p>{data[0]}</p>
-                    <p>1204723601</p>
-                    <p>{data[2]}</p>
-                    <p>{data[4] + " " + data[5] + " " + data[6]}</p>
-                  </div>
-                  <div>
-                    <p className="font-bold text-md">{data[16]} LBS</p>
-                    <p className=" text-sm">DWT:16,8,8</p>
-                  </div>
-                  <div>
-                    <p className="font-bold text-lg"> {index + 1} OF {csvData.length}</p>
-                  </div>
-                </div>
-                <div className="p-1">
-                  <p className="font-bold">SHIP TO:</p>
-                  <div className="ml-3">
-                    <span className="block -mb-1 m-0">{data[8]}</span>
-                    <span className="block -mb-1 m-0">3131378163</span>
-                    <span className="block -mb-1 m-0">{data[10]}</span>
-                    <span className="block font-bold mb-1 text-2xl">
-                    {data[12] + " " + data[13] + " " + data[14]}
-                    </span>
-                  </div>
-                </div>
-                <div className="w-full h-[1px] bg-black"></div>
-                <div className="flex justify-between">
-                  <div className="w-[30%] p-1">
-                    <div className="w-[100px] flex items-center justify-center mx-auto">
+            let trackingId = randomAlphanumeric(18, "uppercase");
+            return (
+              <div className="w-[400px]" key={index} id={`content-id-${index}`}>
+                {/* <button onClick={() => generatePDF(getTargetElement, options)}>Download PDF</button> */}
+                <div
 
+                  className="bg-white border-[3px] border-black "
+                >
+                  <div className="flex flex-row items-start justify-between p-1">
+                    <div className="text-sm ">
+                      <p>{data[0]}</p>
+                      <p>1204723601</p>
+                      <p>{data[2]}</p>
+                      <p>{data[4] + " " + data[5] + " " + data[6]}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold text-md">{data[16]} LBS</p>
+                      <p className=" text-sm">DWT:16,8,8</p>
+                    </div>
+                    <div>
+                      <p className="font-bold text-lg"> {index + 1} OF {csvData.length}</p>
                     </div>
                   </div>
-                  <div className="w-[70%]  border-l-[1px] border-black p-1">
-                    <p className="font-bold text-3xl mb-4">IL 626 9-49</p>
-                    <div className="w-[200px] mx-auto">
-                      <Barcode value="barcode-example" height={50} width={0.9} displayValue={false} />
+                  <div className="p-1">
+                    <p className="font-bold">SHIP TO:</p>
+                    <div className="ml-3">
+                      <span className="block -mb-1 m-0">{data[8]}</span>
+                      <span className="block -mb-1 m-0">3131378163</span>
+                      <span className="block -mb-1 m-0">{data[10]}</span>
+                      <span className="block font-bold mb-1 text-2xl">
+                        {data[12] + " " + data[13] + " " + data[14]}
+                      </span>
                     </div>
                   </div>
-                </div>
-                <div className="w-full h-[6px] bg-black"></div>
-                <div className="flex flex-row items-start justify-between">
-                  <div>
-                    <p className="font-bold text-3xl">UPS 2ND DAY AIR</p>
-                    <p className="">TRACKING #: {trackingId}</p>
+                  <div className="w-full h-[1px] bg-black"></div>
+                  <div className="flex justify-between">
+                    <div className="w-[30%] p-1">
+                      <div className="w-[100px] flex items-center justify-center mx-auto">
+
+                      </div>
+                    </div>
+                    <div className="w-[70%]  border-l-[1px] border-black p-1">
+                      <p className="font-bold text-3xl mb-4">IL 626 9-49</p>
+                      <div className="w-[200px] mx-auto">
+                        <Barcode value="barcode-example" height={50} width={0.9} displayValue={false} />
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    <p className="font-bold text-5xl">2</p>
+                  <div className="w-full h-[6px] bg-black"></div>
+                  <div className="flex flex-row items-start justify-between">
+                    <div>
+                      <p className="font-bold text-3xl">UPS 2ND DAY AIR</p>
+                      <p className="">TRACKING #: {trackingId}</p>
+                    </div>
+                    <div>
+                      <p className="font-bold text-5xl">2</p>
+                    </div>
                   </div>
-                </div>
-                <div className="w-full mt-2 h-[2px] bg-black"></div>
-                <div className="flex justify-center"><Barcode value={trackingId} height={70} width={1} displayValue={false} /></div>
-                <div className="w-full h-[6px] bg-black"></div>
-                <div>
-                  <p className="text-sm">BILLING: P/P</p>
-                  <p className="text-sm">DESC: {data[20]}</p>
-                  <p className="mt-1 font-medium text-sm">
-                    REF #1: {data[21]}
-                  </p>
-                </div>
-                <div className="flex items-end justify-end mt-4 p-1">
-                  <p className="text-sm">ISH 13.00F LASER 15.5V 01/2024</p>
+                  <div className="w-full mt-2 h-[2px] bg-black"></div>
+                  <div className="flex justify-center"><Barcode value={trackingId} height={70} width={1} displayValue={false} /></div>
+                  <div className="w-full h-[6px] bg-black"></div>
+                  <div>
+                    <p className="text-sm">BILLING: P/P</p>
+                    <p className="text-sm">DESC: {data[20]}</p>
+                    <p className="mt-1 font-medium text-sm">
+                      REF #1: {data[21]}
+                    </p>
+                  </div>
+                  <div className="flex items-end justify-end mt-4 p-1">
+                    <p className="text-sm">ISH 13.00F LASER 15.5V 01/2024</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}) : (
+            )
+          }) : (
             ''
           )}
         </div>
